a0b1369c7498b99126745f3f08fe3ca201df2b83
Faust Fu <faust.fu@gmail.com>
2020-07-31 09:15:20 +0800

    Dev: add pm2 settings.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 
-#[get("/{id}/{name}/index.html")]
-async fn index(info: web::Path<(u32, String)>) -> impl Responder {
-    format!("Hello {}! id:{}", info.1, info.0)
+// 1. Use request handlers to extract parameters from a request(trait:FromRequest) and return a response(trait:Responder).
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
 }
 
+// 2. Use path utility to parse path parameters.
+#[get("/{id}/{name}/index.html")]
+async fn index_html(info: web::Path<(u32, String)>) -> impl Responder {
+    HttpResponse::Ok().body(format!("Hello {}! id:{}", info.1, info.0))
+}
+
+// start point
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
+    HttpServer::new(|| App::new().service(index).service(index_html))
         .bind("127.0.0.1:8080")?
         .run()
         .await
